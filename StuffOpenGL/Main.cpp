f8b006a95d42c8a0085b3d6111786a1283740d13
Julian Łabanowski <julian.labanowski@wp.pl>
2024-02-13 00:33:13 +0100

    Messing around with GLM

--- StuffOpenGL/Main.cpp
+++ StuffOpenGL/Main.cpp
@@ -1,5 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Shader.h"
 
@@ -42,15 +47,25 @@ int main()
 	Shader ourShader("Shader.vert", "Shader.frag");
 
 	float vertices[] = {
-		//position				//colors
-		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f, //bottom right
-		-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f, //bottom left
-		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f, //top
+		//position				//colors				//texture coords
+		 0.5f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f,		1.0f, 1.0f,			//top right
+		 0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,			//bottom right
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,		0.0f, 0.0f,			//bottom left
+		-0.5f,  0.5f, 0.0f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f			//top left	
 	};
 
 	unsigned int indices[] = {
-		0, 1, 2
+		0, 1, 2,
+		2, 3, 0
+
 	};
+
+	float texCoords[] = {
+		0.0f, 0.0f, //lower left corner
+		1.0f, 0.0f, //lower right corner
+		0.5f, 1.0f  //top center corner
+	};	
+
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -67,11 +82,85 @@ int main()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	//color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	//texture coords 
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	//set texture wrapping/filtering options to the currently bound gl_texture_2d
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width;
+	int height;
+	int nrChannels;
+
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		//glTexImage2D 1: texture target, 2: mipmap level for texture, 3: format to store texture, 4: width, 5:height 
+		//6:legacy(always 0), 7: format of src image, 8:datatype of src image, 9:actual data
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture " << std::endl;
+	}
+
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	data = stbi_load("chad.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture " << std::endl;
+	}
+
+	ourShader.Use();
+	ourShader.SetInt("texture1", 0);
+	ourShader.SetInt("texture2", 1);
+
+	//glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	//glm::mat4 trans = glm::mat4(1.0f); //identity matrix
+	////trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	////vec = trans * vec;
+	////std::cout << vec.x << vec.y << vec.z << vec.w << std::endl;
+	//trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+	//trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+	//unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+	//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+	//glUniformMatrix4fv 1:uniform's location 2:how many matricies send 3:do we want to transpose matrix,4:data
+	
+
+	//after generating texture and mipmaps its good practice to free image memory
+	stbi_image_free(data);
+
+
 
 	//unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -81,9 +170,23 @@ int main()
 	//uncomment this to draw in wireframe polygons
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+	float xMaxBorder = 1;
+	float posX = 0.1;
+	float xSpeed = 0.01;
+	bool moveRight = true;
+
+	//delta time
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
 	//rendering loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//calculate delta time
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//input
 		processInput(window);
 
@@ -91,19 +194,39 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//bind texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		//activate shader
 		ourShader.Use();
-		//ourShader.SetFloat("someUniform", 1.0f);
+		
+		if (posX < xMaxBorder && moveRight)
+		{
+			posX += xSpeed;
+		}
+		else
+		{
+			moveRight = false;
+			posX -= xSpeed;
+			if (posX < -xMaxBorder)
+				moveRight = true;
+		}
 
-		//update the uniform value
-		//float timeValue = glfwGetTime();
-		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		//matrix transformations
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::scale(trans, glm::vec3(1.0f, 1.0f, 1.0f));
+		trans = glm::translate(trans, glm::vec3(posX, -0.5f, 0.0f));
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		//get matrix's uniform and set value
+		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
 		//render square
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);	
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);	
 
 		//checck and call events and swap buffers
 		glfwSwapBuffers(window);
